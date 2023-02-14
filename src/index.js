c596cbc2a616573341a7656572766a14f9455dc9
Lemon Soju <yckk980320@naver.com>
2023-02-14 17:16:41 +0900

    feat: add navbar

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap-utilities.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
