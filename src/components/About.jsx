89d2bf47e51138acb7d6146e78785ec1ece8ecce
Lemon Soju <yckk980320@naver.com>
2023-02-22 13:44:54 +0900

    feat: draft a about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,134 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <div style={{ margin: "50px" }}>
-        <div className="normal_1">신입 개발자 윤창규입니다.</div>
+      <div
+        className="normal_1"
+        style={{ marginTop: "70px", marginLeft: "20px" }}
+      >
+        신입 개발자 윤창규입니다.
+      </div>
+      <div style={{ marginTop: "50px" }}>
+        <img
+          alt="sketch"
+          src={process.env.PUBLIC_URL + "/sketch.png"}
+          style={{
+            width: "10vw",
+            minWidth: "100px",
+            float: "left",
+            marginLeft: "20px",
+          }}
+        />
+      </div>
+
+      <div
+        className="subhead_2"
+        style={{ marginLeft: "250px", marginBottom: "100px" }}
+      >
+        <div>
+          Contact.
+          <div class="normal_2">
+            <p>Email. yckk980320@naver.com</p>
+            <p>Phone. 010-****-****</p>
+          </div>
+        </div>
+
+        <div style={{ marginTop: "50px" }}>
+          Channel.
+          <div class="normal_2">
+            <p>
+              <a href="http://lemon-soju.tistory.com">
+                Blog. https://lemon-soju.tistory.com/
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/Lemon-soju">
+                GitHub. https://github.com/Lemon-soju
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <hr
+        contenteditable="false"
+        data-ke-type="horizontalRule"
+        data-ke-style="style5"
+      />
+
+      <div class="subhead_1" style={{ marginTop: "150px" }}>
+        Introduce.
+        <div class="normal_1" style={{ marginTop: "50px" }}>
+          <p>
+            자바 스프링 백엔드 개발자를 준비하고 있는 취업 준비생입니다. 어릴
+            적부터 혼자 생각하고 무언가 만드는 것을 좋아했습니다.
+          </p>
+          <p>
+            다른 사람들이 제가 만든 프로그램을 사용한다는 상상을 하면 더욱 더
+            빨리 성장하고 싶은 생각이 듭니다.
+          </p>
+        </div>
+      </div>
+
+      <hr
+        contenteditable="false"
+        data-ke-type="horizontalRule"
+        data-ke-style="style5"
+        style={{ marginTop: "100px" }}
+      />
+
+      <div class="subhead_1" style={{ marginTop: "150px" }}>
+        Project Experience.
+        <div
+          style={{
+            fontSize: "1.75rem",
+            marginTop: "100px",
+            color: "#7e7e7e",
+            textDecoration: "underline",
+          }}
+        >
+          <a href="https://lemon-soju.tistory.com/40">
+            시나리오 기반 모의해킹 프로젝트
+          </a>
+        </div>{" "}
+        <div class="normal_1" style={{ marginTop: "30px" }}>
+          보안프로젝트 모의해킹 실무 과정 30기 팀원들과 함께 시나리오를 구성하여
+          모의해킹 및 침해대응
+        </div>
+        <div class="normal_1" style={{ float: "left" }}>
+          <p style={{ marginTop: "20px" }}>환경구축</p>
+          <p style={{ marginTop: "20px" }}>공격 시나리오 구성 및 공격 수행</p>
+          <p style={{ marginTop: "20px" }}>침해 대응 및 분석</p>
+        </div>
+      </div>
+
+      <hr
+        contenteditable="false"
+        data-ke-type="horizontalRule"
+        data-ke-style="style5"
+        style={{ marginTop: "200px" }}
+      />
+
+      <div class="subhead_1" style={{ marginTop: "150px" }}>
+        Skill.
+        <div class="normal_1" style={{ marginTop: "50px" }}>
+          <p>Front-End: Vue.js</p>
+          <p>Back-End: Spring Boot</p>
+          <p>Database: PostgreSQL</p>
+        </div>
+      </div>
+
+      <hr
+        contenteditable="false"
+        data-ke-type="horizontalRule"
+        data-ke-style="style5"
+        style={{ marginTop: "100px" }}
+      />
+
+      <div class="subhead_1" style={{ marginTop: "150px" }}>
+        Education.
+        <div class="normal_1" style={{ marginTop: "30px" }}>
+          고려대학교 컴퓨터학과 4학년 휴학 중
+        </div>
       </div>
     </>
   );
@@ -12,112 +138,6 @@ const About = () => {
 
 export default About;
 
-// <div style="margin-top: 50px">
-// <img
-//   src="../assets/이력서.jpg"
-//   style="width:10vw; min-width: 100px; float:left"
-// />
-
-// <div
-//   class="subhead_2"
-//   style="margin-left: 250px; margin-bottom: 100px"
-// >
-//   <div>
-//     Contact.
-//     <div class="normal_2">
-//       <p>Email. yckk980320@naver.com</p>
-//       <p>Phone. 010-****-****</p>
-//     </div>
-//   </div>
-
-//   <div style="margin-top: 50px">
-//     Channel.
-//     <div class="normal_2">
-//       <p>
-//         <a href="http://lemon-soju.tistory.com">
-//           Blog. https://lemon-soju.tistory.com/
-//         </a>
-//       </p>
-//       <p>
-//         <a href="https://github.com/Lemon-soju">
-//           GitHub. https://github.com/Lemon-soju
-//         </a>
-//       </p>
-//     </div>
-//   </div>
-// </div>
-
 // {
-/* <hr contenteditable="false" data-ke-type="horizontalRule" data-ke-style="style5">
-
-    <div class="subhead_1" style="margin-top: 150px">
-      Introduce.
-      <div class="normal_1" style="margin-top: 50px">
-        <p>
-          자바 스프링 백엔드 개발자를 준비하고 있는 취업 준비생입니다. 어릴 적부터 혼자 생각하고 무언가 만드는 것을 좋아했습니다.
-        </p>
-        <p>
-          다른 사람들이 제가 만든 프로그램을 사용한다는 상상을 하면 더욱 더 빨리 성장하고 싶은 생각이 듭니다.
-        </p>
-      </div>
-    </div>
-
-    <hr contenteditable="false" data-ke-type="horizontalRule" data-ke-style="style5" style="margin-top: 100px">
-
-    <div class="subhead_1" style="margin-top: 150px">
-      Project Experience.
-
-      <div style="font-size: 1.75rem; margin-top: 100px; color: #7e7e7e; text-decoration: underline">
-        <a href="https://lemon-soju.tistory.com/40">시나리오 기반 모의해킹 프로젝트</a>
-      </div>
-      <div class="normal_1" style="margin-top: 30px">
-        보안프로젝트 모의해킹 실무 과정 30기 팀원들과 함께 시나리오를 구성하여 모의해킹 및 침해대응
-      </div>
-
-      <div class="normal_1" style="float: left;">
-        <p style="margin-top: 20px">환경구축</p>
-        <p style="margin-top: 20px">공격 시나리오 구성 및 공격 수행</p>
-        <p style="margin-top: 20px">침해 대응 및 분석</p>
-      </div>
-
-      <div class="normal_1" style="float: left; margin-top: 10px; margin-left: 100px">
-        <li class="normal_1" style="margin-top: 10px">
-          웹 서버, AD 서버, 방화벽, 웹 방화벽, 통합로그서버, IPS, IDS 인프라 구축
-        </li>
-        <li class="normal_1" style="margin-top: 20px">
-          XSS 취약점을 통해 웹 서버로 1차 침투, hfs 서버 취약점을 이용한 2차 침투 및 랜섬웨어 배포
-        </li>
-        <li class="normal_1" style="margin-top: 20px">
-          통합로그서버 스플렁크를 이용한 분석과 대응방안 모색
-        </li>
-      </div>
-    </div> */
-// }
-
-// {
-/* <hr contenteditable="false" data-ke-type="horizontalRule" data-ke-style="style5" style="margin-top: 200px">
-
-    <div class="subhead_1" style="margin-top: 150px">
-      Skill.
-      <div class="normal_1" style="margin-top: 50px">
-        <p>
-          Front-End: Vue.js
-        </p>
-        <p>
-          Back-End: Spring Boot
-        </p>
-        <p>
-          Database: PostgreSQL
-        </p>
-      </div>
-    </div>
-
-    <hr contenteditable="false" data-ke-type="horizontalRule" data-ke-style="style5" style="margin-top: 100px">
-
-    <div class="subhead_1" style="margin-top: 150px">
-      Education.
-      <div class="normal_1" style="margin-top: 30px">
-        고려대학교 컴퓨터학과 4학년 휴학 중
-      </div>
-    </div> */
+/**/
 // }
