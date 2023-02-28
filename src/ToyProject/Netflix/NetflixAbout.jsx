95a532174b1a99bc85d48df0b28f4eb0e5f0834d
Lemon Soju <yckk980320@naver.com>
2023-02-28 12:57:15 +0900

    feat: add background img to main page

--- src/ToyProject/Netflix/NetflixAbout.jsx
+++ src/ToyProject/Netflix/NetflixAbout.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const NetflixAbout = () => {
-  return <div>About</div>;
+  return (
+    <div style={{ margin: "40px" }}>
+      <p>React.js를 공부하려고 넷플릭스 클론 코딩을 한번 해봤습니다.</p>
+      <p>영화 데이터는 The Movie Database(TMDB)에서 가져왔습니다.</p>
+      <p>프로젝트를 진행하면서 css가 백엔드보다 어렵다는 것을 깨달았습니다.</p>
+    </div>
+  );
 };
 
 export default NetflixAbout;
---
+++ src/components/Developing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Developing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>아직 개발중~~</h1>
+    </div>
+  );
+};
+
+export default Developing;
