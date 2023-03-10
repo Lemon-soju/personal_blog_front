cdaa531abad5b3a4701c97e691ebada8b58f655f
Lemon Soju <yckk980320@naver.com>
2023-03-10 13:08:49 +0900

    feat: add post edit page

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,11 +1,14 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { readPost } from "../controller/controller";
+import { useNavigate } from "react-router-dom";
 
 const PostDetail = () => {
   const params = useParams();
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+
+  const navigate = useNavigate();
 
   const getPost = useCallback(async () => {
     let post = await readPost(params.id);
@@ -23,6 +26,27 @@ const PostDetail = () => {
         <div className="post-title">{title}</div>
       </div>
       <div className="post-content">{content}</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          marginRight: "20%",
+          marginTop: "8vh",
+        }}
+      >
+        <button
+          onClick={(e) =>
+            navigate(`/post/edit/${params.id}`, {
+              state: {
+                title: title,
+                content: content,
+              },
+            })
+          }
+        >
+          수정
+        </button>
+      </div>
     </div>
   );
 };
---
+++ src/components/PostEdit.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { editPost } from "../controller/controller";
+
+const PostEdit = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const params = useParams();
+
+  const editSubmit = async (e) => {
+    e.preventDefault();
+    const data = { id: params.id, title, content };
+    let accessToken = localStorage.getItem("accessToken");
+    let response = await editPost(data, accessToken);
+
+    if (response.status === 200) {
+      window.alert("글 수정 성공");
+      navigate("/");
+      return window.location.reload(); // 최신 글 바로 적용
+    } else {
+      window.alert("글 수정 실패");
+      return window.location.reload();
+    }
+  };
+
+  useEffect(() => {
+    setTitle(location.state.title);
+    setContent(location.state.content);
+  }, [location.state.title, location.state.content]);
+
+  return (
+    <div className="main-body">
+      <form onSubmit={(e) => editSubmit(e)}>
+        <div className="post-create">
+          <input
+            className="post-create-title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div style={{ marginTop: "2vh" }}>
+            <textarea
+              className="post-create-content"
+              value={content}
+              rows={15}
+              onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              marginRight: "26%",
+            }}
+          >
+            <button>완료</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PostEdit;
