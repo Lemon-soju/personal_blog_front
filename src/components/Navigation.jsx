1ce74eaed28c36cfdfc8b012fdbf85e5f39f18bc
Lemon Soju <yckk980320@naver.com>
2023-03-14 21:52:34 +0900

    chore: add uid next to logout button

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { useNavigate } from "react-router-dom";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NetflixNavigation from "../ToyProject/Netflix/NetflixNavigation";
@@ -39,26 +37,25 @@ const Navigation = () => {
               <Nav.Link href="/about">About</Nav.Link>
               <Nav.Link href="/">Home</Nav.Link>
             </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
           </Navbar.Collapse>
         </Container>
         <div className="login-button">
-          <div>
-            <FontAwesomeIcon icon={faUser} />
-          </div>
           <div style={{ marginRight: "3vh", whiteSpace: "nowrap" }}>
             {localStorage.getItem("uid") === null ? (
-              <div onClick={goToLogin}>로그인</div>
+              <>
+                <span style={{ marginRight: "3px" }}>
+                  <FontAwesomeIcon icon={faUser} />
+                </span>
+                <span onClick={goToLogin}>로그인</span>
+              </>
             ) : (
-              <div onClick={goToLogout}>로그아웃</div>
+              <>
+                <span>{localStorage.getItem("uid")}님</span>
+                <button style={{ marginLeft: "10px" }} onClick={goToLogout}>
+                  {" "}
+                  로그아웃
+                </button>
+              </>
             )}
           </div>
         </div>
